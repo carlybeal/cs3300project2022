b93ef0a36a58e3ee95991ff9fccc32f4f5cf9cb9
cbeal <cbeal@uccs.edu>
2022-11-16 23:00:08 +0000

    Iteration 4

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,4 @@
 class Project < ApplicationRecord
     # require title and description
-    #validates_presence_of :title, :description
-    #validates :title, presence: true
-    #validates :description, presence: true
+    validates_presence_of :title, :description
 end
